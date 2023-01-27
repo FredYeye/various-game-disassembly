a73be293f1dc59e4bfafb045a8bcb6f45893229e
Fredrik Sandkvist <fredrik.sandq@gmail.com>
2023-01-27 22:30:02 +0100

    old man & weapons

--- arcade/gng.asm
+++ arcade/gng.asm
@@ -1,6 +1,18 @@
-!arthur_state = 0x8952 ;arthur + 0x01B8 (0x0952)
+;absolute
+!arthur_p1 = 0xFF079A
+!arthur_p2 = 0xFF07FA
 
+;relative to A5(0x8000)
+!transform_old_man_timer = 0x50B4
+!current_player = 0x86D0
+!current_player_arthur_offset = 0x8868
+!invincibility = 0x8931
+!arthur_state = 0x8952 ;arthur + 0x01B8 (0x0952)
+!magic_enabled = 0x895E
+
+;relative to current player/arthur
 !arthur_hp = 0x10
+!arthur_weapon = 0x2C
 
 ;----------
 
@@ -9,38 +21,38 @@
 ;----------
 
 _002664: ;set new weapon
-    movea.l (0x8868, A5), A0 ;0x0868: has offset to arthur
+    movea.l (!current_player_arthur_offset, A5), A0
 .2668:
-    cmp.b   (0x2C, A0), D0
+    cmp.b   (!arthur_weapon, A0), D0
     beq.b   .26A0
 
-    moveq   #0, D1
+    moveq   #0x00, D1
     move.b  D0, D1
-    move.b  (0x2C, A0), (0x2D, A0) ;store old weapon at 0x2D
-    move.b  D0, (0x2C, A0)         ;set new weapon
+    move.b  (!arthur_weapon, A0), (0x2D, A0) ;store old weapon at 0x2D
+    move.b  D0, (!arthur_weapon, A0)         ;set new weapon
     lea     (0x8992, A5), A0 ;0x0992
     move.b  #0x00, (0, A0, D1.w)
     bsr.w   .26B0
-    tst.b   (0x895E, A5) ;0x095E
+    tst.b   (!magic_enabled, A5) ;0x095E
     beq.b   .269A
 
     move.b  #0x00, (0x4052, A5) ;0xC052
-    jsr     0x61DE.w
+    jsr     _00614E.61DE.w
 .269A:
-    jmp     0x614E.l
+    jmp     _00614E.l
 
 .26A0:
     rts
 
 ;probably alternate entry here
-    movea.l (0x8868, A5), A0 ;0x0868
+    movea.l (!current_player_arthur_offset, A5), A0 ;0x0868
     move.w  #0x0000, D0
     move.b  (0x2D, A0), D0
     bra.b   .2668
 
 .26B0:
     move.b  #0xFF, (0x885A, A5) ;0x085A
-    moveq   #0, D0
+    moveq   #0x00, D0
     move.b  D0, (0x885B, A5) ;0x085B
     move.w  D0, (0x885C, A5) ;0x085C
     move.b  D0, (0x50E2, A5) ;0xD0E2
@@ -50,18 +62,42 @@ _002664: ;set new weapon
 
 ;----------
 
-_006194:
+_00614E:
+    movea.l #!arthur_p1, A4
+    tst.b   (!current_player, A5)
+    beq.b   .6160
+
+    movea.l #!arthur_p2, A4
+.6160:
+    tst.b   (0x8768, A5)
+    beq.w   .6178
+
+    moveq   #0x00, D0
+    move.b  (!arthur_weapon, A4), D0
+    bsr.w   .64E8
+    moveq   #0x0E, D0
+    bra.w   .64E8
+
+.6178:
+    moveq   #0x00, D0
+    move.b  (!arthur_weapon, A4), D0
+    bra.w   .674A
+
+;---
+
+.6194:
     ;todo
 
 ;---
 
 .61C0:
-    movea.l #0xFF079A, A4
-    tst.b   (0x86D0, A5)
+    movea.l #!arthur_p1, A4
+    tst.b   (!current_player, A5)
     beq.b   .61D2
-    movea.l #0xFF07FA, A4
+
+    movea.l #!arthur_p2, A4
 .61D2:
-    moveq   #0, D1
+    moveq   #0x00, D1
     move.b  (0x2C, A4), D1
     add.w   D1, D0
     bra.w   .674A
@@ -69,7 +105,7 @@ _006194:
 ;---
 
 .61DE:
-    moveq   #0, D0
+    moveq   #0x00, D0
     move.b  (0x4052, A5), D0
     move.w  (0x06, PC, D0.w), D1
     jmp     (0x02, PC, D1.w)
@@ -79,7 +115,7 @@ _006194:
 .61F0: ;0x0004
     ;gold armor stuff
     addq.b  #2, (0x4052, A5)
-    moveq   #0, D0
+    moveq   #0x00, D0
     move.b  D0, (0x4053, A5)
     move.b  D0, (0x4057, A5)
     move.b  D0, (0x4058, A5)
@@ -90,14 +126,14 @@ _006194:
     move.b  D0, (0x8962, A5)
     move.b  D0, (0x8961, A5)
     move.b  D0, (0x895F, A5)
-    move.b  #0x01, (0x895E, A5)
+    move.b  #0x01, (!magic_enabled, A5)
     tst.b   (0x8768, A5)
     beq.w   .6246
 
     move.w  #0x000F, D0
     bsr.w   .64E8
     move.w  #0x0000, D0
-    bsr.w   _006194
+    bsr.w   .6194
     bsr.w   .644C
     bra.w   .638A
 
@@ -120,7 +156,7 @@ _006194:
 .63D8: ;draws the other dots
     movea.l #0x90036C, A0
     move.w  #0x0001, D2
-    moveq   #0, D0
+    moveq   #0x00, D0
     move.b  (0x4057, A5), D0
     add.b   D0, D0
     lea     (0x0B4C, PC), A4 ;A4 = 0x6F38
@@ -142,11 +178,11 @@ _006194:
 ;---
 
 .644C:
-    moveq   #0, D0
+    moveq   #0x00, D0
     move.b  (0x4057, A5), D0
     move.b  (0x0E, PC, D0.w), D0
     move.b  D0, (0x4054, A5)
-    moveq   #0, D1
+    moveq   #0x00, D1
     rts
 
 ;---
@@ -156,14 +192,14 @@ _006194:
 
 ;---
 
-.674A: ;draw red dot closest to the weapon icon and redraw weapon icon
+.674A: ;draw red dot closest to the weapon icon and/or redraw weapon icon
     lsl.b   #1, D0
     lea     (0x80, PC), A0 ;A0 = 0x67CE
     move.w  (0, A0, D0.w), D0
     lea     (0, A0, D0.w), A0
 .6758:
-    moveq   #0, D0
-    moveq   #0, D2
+    moveq   #0x00, D0
+    moveq   #0x00, D2
     move.l  D2, D1
     lea     0x900000, A2
     move.b  (A0)+, D2
@@ -226,15 +262,70 @@ arthur: ;unknown start; placeholder label to have a label to attach sub labels t
 ;---
 
 .old_man_init: ;D09E
-    moveq   #0, D0
+    moveq   #0x00, D0
     move.b  D0, (0x50B0, A5)
     move.b  D0, (0x50B1, A5)
     move.l  #.old_man_run, (!arthur_state, A5)
 .old_man_run: ;D0B0
     btst.b  #0x03, (0x08, A1)
     beq.b   .D0CC
-    ;todo
+
+    move.b  #0x00, (0x8963, A5)
+    move.l  #0xD2B6, (0x8952, A5)
+    jmp     0xD2B6.l
+
 .D0CC:
+    tst.b   (0x885C, A5)
+    beq.b   .D0D6
+
+    addq.b  #1, (0x885D, A5)
+.D0D6:
+    tst.b   (0x50E2, A5)
+    beq.b   .D0E0
+
+    subq.b #1, (0x50E2, A5)
+.D0E0:
+    moveq   #0x00, D0
+    move.b  (0x50B0, A5), D0
+    move.w  (0x0C, PC, D0.w), D0
+    jsr     (0x08, PC, D0.w)
+    movea.l (0x8966, A5), A0
+    jmp     (A0)
+
+    d16 0x0006, 0x003E, 0x0050
+
+;---
+
+.D0FA: ;0006
+    movea.l #0x0141A8, A2
+    move.b  #0x00, (0x11, A1)
+    move.b  #0x00, (0x12, A1)
+    movea.l (0x28, A1), A0
+    suba.w  #0x0180, A0
+    move.b  (0x0F, A1), D2
+    move.l  A0, (0x28, A1)
+    jsr     0x2880.w
+    move.b  D2, (0x0F, A1)
+    move.w  #0x017D, (!transform_old_man_timer, A5)
+    addq.b  #2, (0x50B0, A5)
+    jmp 0x46DA.w
+
+;---
+
+.D132: ;003E
+    tst.w   (!transform_old_man_timer, A5)
+    beq.b   .D13E
+
+    subq.w  #1, (!transform_old_man_timer, A5)
+    rts
+
+.D13E:
+    addq.b  #2, (0x50B0, A5)
+    rts
+
+;---
+
+.D144: ;0050
     ;todo
 
 ;---
@@ -254,7 +345,7 @@ arthur: ;unknown start; placeholder label to have a label to attach sub labels t
     jsr     0x2880.w
     move.b  D2, (0x0F, A1)
     move.l  #arthur.B5C2, (!arthur_state, A5)
-    move.b  #0x00, (0x8931, A5)
+    move.b  #0x00, (!invincibility, A5)
     jsr     0x466A.w
     movea.l (0x8966, A5), A0
     jmp     (A0)
@@ -281,7 +372,7 @@ arthur: ;unknown start; placeholder label to have a label to attach sub labels t
     jmp     0xBC14.l
 
 .D572:
-    moveq   #0, D0
+    moveq   #0x00, D0
     move.b  (0x50B6, A5), D0
     move.w  (0x0C, PC, D0.w), D0
     jsr     (0x08, PC, D0.w)
@@ -314,7 +405,7 @@ arthur: ;unknown start; placeholder label to have a label to attach sub labels t
 
 ;D72E (0008) - standing duck?
     move.b  #0x00, (0x8956, A5)
-    moveq   #0, D0
+    moveq   #0x00, D0
     move.l  D0, (0x14, A1)
     move.b  D0, (0x0E, A1)
     btst.b  #0x00, (0x09, A1) ;check flags
@@ -357,20 +448,20 @@ arthur: ;unknown start; placeholder label to have a label to attach sub labels t
 ;----------
 
 _01B714: ;gold armor stuff
-    movea.l (0x8868, A5), A0
+    movea.l (!current_player_arthur_offset, A5), A0
     move.b  #0x02, (0x11, A0)
     move.b  #0x03, (0x12, A0)
     move.b  #0x00, (0x4052, A5)
-    jsr     _006194.61DE ;creates magic bar
+    jsr     _00614E.61DE ;creates magic bar
     move.l  #0xC920, (0x8966, A5)
-    move.b  #0xFF, (0x8931, A5)
+    move.b  #0xFF, (!invincibility, A5)
     jmp     0x466A.w
 
 ;----------
 
 _0489B0: ;armor upgrade
     andi.b  #0xDF, (0x13, A1)
-    movea.l (0x8868, A5), A2
+    movea.l (!current_player_arthur_offset, A5), A2
     cmpi.b  #0x03, (0x12, A2)
     beq.w   .8A14
 
@@ -413,7 +504,7 @@ _0489B0: ;armor upgrade
 ; A2: magician object slot
 
 _0496B2: ;create magician projectile
-    moveq   #0, D0
+    moveq   #0x00, D0
     lea     (0x40, A0), A0
     move.w  #0x0003, D1
 .96BC:
@@ -449,7 +540,7 @@ _0496B2: ;create magician projectile
     rts
 
 .975A:
-    moveq   #0, D0
+    moveq   #0x00, D0
     move.b  (0x41, A1), D0 ;use direction as offset
     lsl.w   #2, D0
     lea     (0x10, PC, D0.w), A0
@@ -459,20 +550,20 @@ _0496B2: ;create magician projectile
 
 ;---
 
-_049818: ;magician projectile hitting arthur?
+_049818: ;magician projectile hitting arthur
     andi.b  #0xDF, (0x13, A1)
-    tst.b   (0x8931, A5) ;0x0931: arthur + 0x197 (i frames active?)
+    tst.b   (!invincibility, A5)
     bne.w   .9884
 
     clr.b   (0x13, A1)
-    movea.l (0x8868, A5), A2 ;0x0868: arthur + 0xCE (arthur base offset)
-    tst.b   (0x10, A2) ;check arthur hp
+    movea.l (!current_player_arthur_offset, A5), A2
+    tst.b   (!arthur_hp, A2)
     beq.w   .983E
 
     bsr.w   .988A
     bra.w   .9842
 
-.983E: ;0 hp
+.983E: ;0 hp (underwear)
     bsr.w   .98A2
 .9842:
     move.b  #0x29, (0x12, A1)
